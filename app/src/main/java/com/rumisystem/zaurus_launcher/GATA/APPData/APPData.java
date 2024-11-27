6fb583466f19ecd0191b7a62ba7aee569613f376
SINtyanneru <irjitgd@gmail.com>
2024-11-27 11:35:03 +0900

    全消ししてレイアウト作成まで

--- app/src/main/java/com/rumisystem/zaurus_launcher/GATA/APPData/APPData.java
+++
@@ -1,8 +0,0 @@
-package com.rumisystem.zaurus_launcher.GATA.APPData;
-
-public class APPData {
-	public String PACKAGE_NAME = "com.example";
-	public String NAME = "Example";
-	public int API_LEVEL = 0;
-	public int CATEGORY = 0;
-}
